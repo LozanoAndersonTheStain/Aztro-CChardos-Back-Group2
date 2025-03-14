dad84dd3312b8bb869475061c13d241831c094d2
Anderson Lozano <adslozanov@gmail.com>
2025-03-14 10:29:24 -0500

    🔨 The user entity is created to define the table structure and its constraints.

---
+++ Domain/Entities/UserEntity.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace aztro_cchardos_back_group2.Domain.Entities
+{
+    [Table("Users")]
+    public class UserEntity
+    {
+        [Key]
+        [Column("id")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Column("name")]
+        [Required, StringLength(50, MinimumLength = 3, ErrorMessage = "Name is required")]
+        public string Name { get; set; } = "";
+
+        [Column("email")]
+        [Required, EmailAddress(ErrorMessage = "Email is invalid"), StringLength(50, MinimumLength = 3, ErrorMessage = "Email is required")]
+        public string Email { get; set; } = "";
+
+        [Column("password")]
+        [Required, StringLength(80, MinimumLength = 3, ErrorMessage = "Password is required")]
+        public string Password { get; set; } = "";
+
+        [Column("role")]
+        [Required, StringLength(50, MinimumLength = 3, ErrorMessage = "Role is required")]
+        public string Role { get; set; } = "";
+    }
+}
