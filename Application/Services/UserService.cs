2ef6574c50f3fc380505a61e6990716fbbe29657
Anderson Lozano <adslozanov@gmail.com>
2025-03-14 18:56:49 -0500

    🔨 Password encryption added to services

--- Application/Services/UserService.cs
+++ Application/Services/UserService.cs
@@ -14,10 +14,11 @@ namespace aztro_cchardos_back_group2.Application.Services
         public async Task<UserResponse> CreateUserAsync(UserRequest userRequest)
         {
             var user = _mapper.Map<UserEntity>(userRequest); //* Mapea el UserRequest a UserEntity
+            user.Password = BCrypt.Net.BCrypt.HashPassword(userRequest.Password); //* Encripta la contraseña
             var createdUser = await _userRepository.CreateUserAsync(user) ?? throw new Exception("User not created"); //* Crea el usuario o lanza una excepción si no se crea el usuario
             var response = _mapper.Map<UserResponse>(createdUser); //* Mapea el usuario creado a UserResponse
             response.Success = true; //* Establece la propiedad Success en true
-            return response;
+            return response; //* Retorna el usuario creado
         }
 
         public async Task<UserResponse> RegisterUserAsync(UserRequest userRequest)
@@ -26,6 +27,7 @@ namespace aztro_cchardos_back_group2.Application.Services
             if (existingUser != null) throw new Exception("User already exists"); //* Verifica si el usuario ya existe
 
             var user = _mapper.Map<UserEntity>(userRequest); //* Mapea el UserRequest a UserEntity
+            user.Password = BCrypt.Net.BCrypt.HashPassword(userRequest.Password); //* Encripta la contraseña
             var createdUser = await _userRepository.CreateUserAsync(user) ?? throw new Exception("User not created"); //* Crea el usuario o lanza una excepción si no se crea el usuario
             var response = _mapper.Map<UserResponse>(createdUser); //* Mapea el usuario creado a UserResponse
             response.Success = true; //* Establece la propiedad Success en true
@@ -35,10 +37,10 @@ namespace aztro_cchardos_back_group2.Application.Services
         public async Task<UserResponse> LoginUserAsync(string email, string password)
         {
             var user = await _userRepository.GetUserByEmailAsync(email) ?? throw new Exception("User not found"); //* Obtiene el usuario por email o lanza una excepción si no se encuentra el usuario
-            if (user.Password != password) throw new Exception("Invalid password"); //* Verifica la contraseña del usuario
+            if (!BCrypt.Net.BCrypt.Verify(password, user.Password)) throw new Exception("Invalid password"); //* Verifica la contraseña encriptada del usuario
             var response = _mapper.Map<UserResponse>(user); //* Mapea el usuario a UserResponse
             response.Success = true; //* Establece la propiedad Success en true
-            return response;
+            return response; //* Retorna el usuario
         }
 
         public async Task<UserResponse> GetUserByIdAsync(int id)
@@ -68,10 +70,11 @@ namespace aztro_cchardos_back_group2.Application.Services
         public async Task<UserResponse> UpdateUserAsync(int id, UserRequest userRequest)
         {
             var user = _mapper.Map<UserEntity>(userRequest); //* Mapea el UserRequest a UserEntity
+            user.Password = BCrypt.Net.BCrypt.HashPassword(userRequest.Password); //* Encripta la contraseña
             var updatedUser = await _userRepository.UpdateUserAsync(id, user) ?? throw new Exception("User not updated"); //* Actualiza el usuario o lanza una excepción si no se actualiza el usuario
             var response = _mapper.Map<UserResponse>(updatedUser); //* Mapea el usuario actualizado a UserResponse
             response.Success = true; //* Establece la propiedad Success en true
-            return response;
+            return response; //* Retorna el usuario actualizado
         }
 
         public async Task<UserResponse> DeleteUserAsync(int id)
