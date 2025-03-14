facfc5e04e9924eaab01fe21d9234fb05b2e1199
Anderson Lozano <adslozanov@gmail.com>
2025-03-14 12:12:45 -0500

    🔨 Interfaces are created to define the methods to be implemented in UserRepository and UserService.

---
+++ Domain/interfaces/IUserRepository.cs
@@ -0,0 +1,28 @@
+using aztro_cchardos_back_group2.Domain.Entities;
+
+namespace aztro_cchardos_back_group2.Domain.Interfaces
+{
+    //* Interfaz para el repositorio de usuarios
+    public interface IUserRepository {
+        //* Metodo para crear un usuario
+        Task<UserEntity> CreateUserAsync(UserEntity user);
+
+        //* Metodo para obtener un usuario por id
+        Task<UserEntity?> GetUserByIdAsync(int id);
+
+        //* Metodo para obtener todos los usuarios
+        Task<List<UserEntity>> GetAllUsersAsync();
+
+        //* Metodo para obtener un usuario por email
+        Task<UserEntity?> GetUserByEmailAsync(string email);
+
+        //* Metodo para obtener los usuarios paginados
+        Task<List<UserEntity>> GetUserPaginatedAsync(int page, int pageSize);
+
+        //* Metodo para actualizar un usuario
+        Task<UserEntity> UpdateUserAsync(int id, UserEntity user);
+
+        //* Metodo para eliminar un usuario
+        Task<bool> DeleteUserAsync(int id);
+    }
+}
---
+++ Domain/interfaces/IUserService.cs
@@ -0,0 +1,18 @@
+using aztro_cchardos_back_group2.Application.DTOs.Requests;
+using aztro_cchardos_back_group2.Application.DTOs.Responses;
+
+namespace aztro_cchardos_back_group2.Domain.Interfaces
+{
+    public interface IUserService {
+        //* Metodos que se van a implementar en la clase UserService
+        Task<UserResponse> RegisterUserAsync(UserRequest request); //* Metodo para registrar un usuario 
+        Task<UserResponse> LoginUserAsync(UserRequest request); //* Metodo para loguear un usuario
+        Task<UserResponse> CreateUserAsync(UserRequest request); //* Metodo para crear un usuario
+        Task<UserResponse> GetUserByIdAsync(int id); //* Metodo para obtener un usuario por su id
+        Task<List<UserResponse>> GetAllUsersAsync(); //* Metodo para obtener todos los usuarios
+        Task<UserResponse> GetUserByEmailAsync(string email); //* Metodo para obtener un usuario por su email
+        Task<UserResponse> GetUserPaginatedAsync(int page, int pageSize); //* Metodo para obtener un usuario paginado
+        Task<UserResponse> UpdateUserAsync(int id, UserRequest request); //* Metodo para actualizar un usuario
+        Task<UserResponse> DeleteUserAsync(int id); //* Metodo para eliminar un usuario
+    }
+}
