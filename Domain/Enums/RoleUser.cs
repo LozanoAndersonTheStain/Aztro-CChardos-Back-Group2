74e54e6199fe4bb5ebfb7cb0663c416d171b00be
Anderson Lozano <adslozanov@gmail.com>
2025-03-14 10:36:04 -0500

    🔨 The RoleUser class is created for the different roles that the users will have.

---
+++ Domain/Enums/RoleUser.cs
@@ -0,0 +1,7 @@
+namespace aztro_cchardos_back_group2.Domain.Enums {
+    public static class RoleUser
+    {
+        public const string Admin = "Admin";
+        public const string User = "User"; 
+    }
+}
