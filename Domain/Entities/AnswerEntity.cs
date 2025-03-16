7f2e34e001d7b47b59000864cd29d18aaa9129cc
Anderson Lozano <adslozanov@gmail.com>
2025-03-16 08:44:19 -0500

    🔨 The entity for the answer is created.

---
+++ Domain/Entities/AnswerEntity.cs
@@ -0,0 +1,35 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace aztro_cchardos_back_group2.Domain.Entities
+{
+    [Table("answers")]
+    public class AnswerEntity
+    {
+        [Key]
+        [Column("id")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Column("user_id")]
+        public int UserId { get; set; }
+
+        [Column("question_id")]
+        public int QuestionId { get; set; }
+
+        [Column("question_option_id")]
+        public int QuestionOptionId { get; set; }
+
+        [Column("date")]
+        public DateTime Date { get; set; } = DateTime.Now;
+
+        [ForeignKey("UserId")]
+        public virtual UserEntity User { get; set; } = null!;
+
+        [ForeignKey("QuestionId")]
+        public virtual QuestionEntity Question { get; set; } = null!;
+
+        [ForeignKey("QuestionOptionId")]
+        public virtual QuestionOptionEntity QuestionOption { get; set; } = null!;
+    }
+}
