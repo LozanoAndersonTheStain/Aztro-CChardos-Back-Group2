479b67ac0d77370b17369df4855f5cd187ed61d6
Anderson Lozano <adslozanov@gmail.com>
2025-03-16 12:17:37 -0500

    🔨 The entity for the Destinationis created.

---
+++ Domain/Entities/DestinationEntity.cs
@@ -0,0 +1,29 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace aztro_cchardos_back_group2.Domain.Entities
+{
+    public class DestinationEntity
+    {
+        [Key]
+        [Column("id")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Column("combination")]
+        [Required]
+        public string Combination { get; set; } = "";
+
+        [Column("first_city_id")]
+        public int FirstCityId { get; set; }
+
+        [Column("second_city_id")]
+        public int SecondCityId { get; set; }
+
+        [ForeignKey("FirstCityId")]
+        public virtual CityEntity FirstCity { get; set; } = null!;
+
+        [ForeignKey("SecondCityId")]
+        public virtual CityEntity SecondCity { get; set; } = null!;
+    }
+}
