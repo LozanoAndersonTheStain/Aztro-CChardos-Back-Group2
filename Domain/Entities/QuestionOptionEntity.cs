17f586c0807fe6243d47806acdcd06a27886df4c
Anderson Lozano <adslozanov@gmail.com>
2025-03-15 22:39:49 -0500

    🔨 Entity is created for the question option.

---
+++ Domain/Entities/QuestionOptionEntity.cs
@@ -0,0 +1,32 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace aztro_cchardos_back_group2.Domain.Entities
+{
+    [Table("QuestionOptions")]
+    public class QuestionOptionEntity
+    {
+        [Key]
+        [Column("id")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Column("question_id")]
+        public int QuestionId { get; set; }
+
+        [Column("description")]
+        [Required]
+        [MaxLength(255)]
+        public string Description { get; set; } = "";
+
+        [Column("image")]
+        [Required]
+        [MaxLength(500)]
+        public string Image { get; set; } = "";
+
+        [ForeignKey("QuestionId")]
+        public virtual QuestionEntity Question { get; set; } = null!;
+
+        public virtual ICollection<AnswerEntity> Answers { get; set; } = [];
+    }
+}
