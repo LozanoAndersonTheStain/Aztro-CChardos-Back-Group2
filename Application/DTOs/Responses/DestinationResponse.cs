b9736abd0d352fccb0b1bbab6f1e0743b3f936df
Anderson Lozano <adslozanov@gmail.com>
2025-03-16 18:53:26 -0500

    🔨 The response returned by the DestinationResponse is modified to show the destinations

--- Application/DTOs/Responses/DestinationResponse.cs
+++ Application/DTOs/Responses/DestinationResponse.cs
@@ -2,10 +2,8 @@ namespace aztro_cchardos_back_group2.Application.DTOs.Responses
 {
     public class DestinationResponse
     {
-        public int Id { get; set; }
-        public string Combination { get; set; } = "";
-        public int FirstCityId { get; set; }
-        public int SecondCityId { get; set; }
+        public CityResponse FirstCity { get; set; } = null!;
+        public CityResponse SecondCity { get; set; } = null!;
         public bool Success { get; set; }
         public string Message { get; set; } = "";
     }
