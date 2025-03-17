7b57e84f0edf9793eb90669553249f6eb576c0aa
Anderson Lozano <adslozanov@gmail.com>
2025-03-16 19:41:56 -0500

    🔨 Added DTO for multiple combinations creation.

---
+++ Application/DTOs/Requests/CreateCombinationsRequest.cs
@@ -0,0 +1,7 @@
+namespace aztro_cchardos_back_group2.Application.DTOs.Requests
+{
+    public class CreateCombinationsRequest
+    {
+        public List<CombinationRequest> Combinations { get; set; } = [];
+    }
+}
