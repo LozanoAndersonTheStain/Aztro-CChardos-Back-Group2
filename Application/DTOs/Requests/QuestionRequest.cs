1174a7ba80f7ca91d849af250c5929b25d555bd3
Anderson Lozano <adslozanov@gmail.com>
2025-03-18 21:23:13 -0500

    🔨 The request and response dts are modified to add the new assigned properties.

--- Application/DTOs/Requests/QuestionRequest.cs
+++ Application/DTOs/Requests/QuestionRequest.cs
@@ -2,7 +2,9 @@ namespace aztro_cchardos_back_group2.Application.DTOs.Requests
 {
     public class QuestionRequest
     {
+        public string Category { get; set; } = "";
         public string QuestionText { get; set; } = "";
+        public string SupplementaryText { get; set; } = "";
         public List<QuestionOptionRequest> Options { get; set; } = [];
     }
 }
--- Application/DTOs/Responses/QuestionResponse.cs
+++ Application/DTOs/Responses/QuestionResponse.cs
@@ -3,7 +3,9 @@ namespace aztro_cchardos_back_group2.Application.DTOs.Responses
     public class QuestionResponse
     {
         public int Id { get; set; }
+        public string Category { get; set; } = "";
         public string QuestionText { get; set; } = "";
+        public string SupplementaryText { get; set; } = "";
         public List<QuestionOptionResponse> Options { get; set; } = [];
         public bool Success { get; set; }
         public string Message { get; set; } = "";
