3e39e45b95aec3407c4f22e3b8a383655b530c29
Anderson Lozano <adslozanov@gmail.com>
2025-03-18 21:35:03 -0500

    🔨 New dto is added, to create questions by the name of the category to which it will be assigned.

---
+++ Application/DTOs/Requests/QuestionGroupRequest.cs
@@ -0,0 +1,7 @@
+namespace aztro_cchardos_back_group2.Application.DTOs.Requests
+{
+    public class QuestionGroupRequest
+    {
+        public List<QuestionRequest> Questions { get; set; } = [];
+    }
+}
