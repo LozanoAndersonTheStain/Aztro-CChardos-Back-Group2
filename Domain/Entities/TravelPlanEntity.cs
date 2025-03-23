f134473f309190feb6596baec496ae3d927deaf6
Anderson Lozano <adslozanov@gmail.com>
2025-03-23 15:08:20 -0500

    🔨 The TravelPlan entity is added which will assign the travel plan you have for each destination.

---
+++ Domain/Entities/TravelPlanEntity.cs
@@ -0,0 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace aztro_cchardos_back_group2.Domain.Entities
+{
+    [Table("Travel Plans")]
+    public class TravelPlanEntity
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("name")]
+        [Required]
+        public string Name { get; set; } = "";
+
+        [Column("description")]
+        [Required]
+        public string DestinationName { get; set; } = "";
+
+        [Column("image")]
+        [Required]
+        public string Image { get; set; } = "";
+
+        public virtual ICollection<TransportOptionEntity> FlightOptions { get; set; } = [];
+        public virtual ICollection<AccommodationOptionEntity> HotelOptions { get; set; } = [];
+    }
+}
