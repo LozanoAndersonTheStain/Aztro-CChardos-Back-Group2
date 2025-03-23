a2f0fc7b5ef28c538175c9536b27fedcd54ddbdf
Anderson Lozano <adslozanov@gmail.com>
2025-03-23 15:11:08 -0500

    🔨 The AccommodationOptions entity is created, which will define the accommodation options for each plan, for the moment only hotels.

---
+++ Domain/Entities/AccommodationOptionEntity.cs
@@ -0,0 +1,36 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace aztro_cchardos_back_group2.Domain.Entities
+{
+    [Table("AccommodationOptions")]
+    public class AccommodationOptionEntity
+    {
+        [Key]
+        [Column("id")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Column("name")]
+        [Required]
+        public string Name { get; set; } = "";
+
+        [Column("description")]
+        [Required]
+        public string Description { get; set; } = "";
+
+        [Column("image_url")]
+        [Required]
+        public string ImageUrl { get; set; } = "";
+
+        [Column("url")]
+        [Required]
+        public string Url { get; set; } = "";
+
+        [Column("travel_plan_id")]
+        public int TravelPlanId { get; set; }
+
+        [ForeignKey("TravelPlanId")]
+        public virtual TravelPlanEntity TravelPlan { get; set; } = null!;
+    }
+}
