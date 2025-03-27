dee8c9c41ff954bdb46d0392454062e2cd521019
Anderson Lozano <adslozanov@gmail.com>
2025-03-27 00:53:23 -0500

    🔨 The relevant fields are added to return the information of the plan assigned to each destination.

--- Application/DTOs/Responses/DestinationResponse.cs
+++ Application/DTOs/Responses/DestinationResponse.cs
@@ -4,6 +4,8 @@ namespace aztro_cchardos_back_group2.Application.DTOs.Responses
     {
         public CityResponse FirstCity { get; set; } = null!;
         public CityResponse SecondCity { get; set; } = null!;
+        public TravelPlanResponse FirstCityTravelPlan { get; set; } = null!;
+        public TravelPlanResponse SecondCityTravelPlan { get; set; } = null!;
         public bool Success { get; set; }
         public string Message { get; set; } = "";
     }
